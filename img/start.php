537ddf621b0fd8274eddf4e018f86a392a4703e8
Ivan <Zinchenko.ivan17@mail.ru>
2016-06-21 12:13:10 +0200

    dis

---
+++ img/start.php
@@ -0,0 +1,14 @@
+<?php
+$a=10;
+$b=20;
+$c=30;
+if(($a*$b)>100)
+    echo "Start";
+?>
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
--- readme.md
+++
@@ -1,7 +0,0 @@
-#php1603
-my dev test repository
- HEAD
-my start next chengess
-
-start2 changes
- start2
